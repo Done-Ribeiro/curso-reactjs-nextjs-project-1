6bc3f28e8b14ebdf755f1bf19c446c5f53b4683b
Done Ribeiro <www.done_@hotmail.com>
2023-02-11 18:14:20 -0300

    Estado com arrays

--- src/App.js
+++ src/App.js
@@ -1,87 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
 import { Component } from 'react';
+import './App.css';
 
-// React - componentes de Classes
-// stateless -> não tem estado
-//
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  // (1)*
-  //   this.handlePClick = this.handlePClick.bind(this);
-
-  //   this.state = {
-  //     name: 'Done Ribeiro',
-  //     counter: 0,
-  //   };
-  // }
-
-  // nao precisa do construtor se o state for declarado como um: class Fields.. desta forma abaixo
   state = {
-    name: 'Done Ribeiro',
-    counter: 0,
-  };
-
-  // (1)*
-  // não precisa do bind, se nos nossos métodos usarmos arrow functions
-  handlePClick = () => {
-    this.setState({ name: 'Júnior' });
-  };
-
-  handleAClick = (event) => {
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1 });
+    posts: [
+      {
+        id: 1,
+        title: 'O título 1',
+        body: 'O corpo 1',
+      },
+      {
+        id: 2,
+        title: 'O título 2',
+        body: 'O corpo 2',
+      },
+      {
+        id: 3,
+        title: 'O título 3',
+        body: 'O corpo 3',
+      },
+    ],
   };
 
   render() {
-    const { name, counter } = this.state;
+    const { posts } = this.state;
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClick}>
-            {name} {counter}
-          </p>
-          <a
-            onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Este é o link
-          </a>
-        </header>
+        {posts.map((post) => (
+          // a key precisa sempre estar ligada ao pai, nesse caso a div
+          // poderia ser um fragmento por exemplo: <>...</>
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
 }
-
-// React - componentes de Função
-// stateless -> não tem estado
-//
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
