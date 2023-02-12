f55dcd517983c5321f1a1acab85fa531630327bb
Done Ribeiro <www.done_@hotmail.com>
2023-02-12 15:59:20 -0300

    Props e organização - parte 2

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
 import { Component } from 'react';
+
 import './App.css';
-import { PostCard } from './components/PostCard';
+
+import { loadPosts } from './utils/load-posts';
+import { Posts } from './components/Posts';
 
 class App extends Component {
   state = {
     posts: [],
   };
 
-  // Montagem
-  componentDidMount() {
-    this.loadPosts();
+  async componentDidMount() {
+    await this.loadPosts();
   }
 
   loadPosts = async () => {
-    const postResponse = fetch('https://jsonplaceholder.typicode.com/posts');
-    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
-
-    const [posts, photos] = await Promise.all([postResponse, photosResponse]);
-
-    const postsJson = await posts.json();
-    const photosJson = await photos.json();
-
-    const postsAndPhotos = postsJson.map((post, index) => {
-      return { ...post, cover: photosJson[index].url };
-    });
-
+    const postsAndPhotos = await loadPosts();
     this.setState({ posts: postsAndPhotos });
   };
 
@@ -33,17 +24,7 @@ class App extends Component {
 
     return (
       <section className="container">
-        <div className="posts">
-          {posts.map((post) => (
-            <PostCard
-              key={post.id}
-              title={post.title}
-              body={post.body}
-              id={post.id}
-              cover={post.cover}
-            />
-          ))}
-        </div>
+        <Posts posts={posts} />
       </section>
     );
   }
